9ec10a12ac3e147ced27d2a438c01710d25890ab
DudaSSilva <ess47@aluno.ifal.edu.br>
2022-09-06 15:39:15 -0300

    feat: setting 'perdeu' (lost) function

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function personagemPular() {
-    const personagem = document.getElementById("personagem")
-    const personagemEstilosCSS = getComputedStyle(personagem)
+const personagem = document.getElementById("personagem")
+const personagemEstilosCSS = getComputedStyle(personagem)
+
+const bloco = document.getElementById("bloco")
+const blocoEstilosCSS = getComputedStyle(bloco)
+
 const personagemPular = () => {    
     const posicaoDoBottom = personagemEstilosCSS.bottom.split("px")[0]
     if(posicaoDoBottom == "0") {
@@ -10,6 +13,22 @@ const personagemPular = () => {
         setTimeout(()=>{
             personagem.classList.remove(pularClasseCSS)
         }, tempoDaAnimacao * 1000)
-       
     }
 }
+
+const perdeu = setInterval(()=>{
+    let gameWidth = parseInt(getComputedStyle(document.getElementById("jogo")).width)
+    let rightPersonagem = parseInt(personagemEstilosCSS.right.split("px")[0])
+    let leftPersonagem = gameWidth - rightPersonagem
+    let leftBloco = parseInt(personagemEstilosCSS.left.split("px")[0])
+    
+    console.log(gameWidth);
+    console.log(rightPersonagem);
+    console.log(leftPersonagem);
+    if(leftBloco >= leftPersonagem) {
+        console.log('perdeu');
+        return true
+    } else {
+        return false
+    }
+}, 1000);
